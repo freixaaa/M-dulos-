851dbf49523ddb629423c5651487f89196dcc8bc
freixaaa <fiorella07freixa23.r@gmail.com>
2026-03-13 21:51:41 -0600

    Update mascota.py

--- modulos/mascota.py
+++ modulos/mascota.py
@@ -1,8 +1,30 @@
 class mascota:
+
     def __init__(self, nombre, especie, edad):
-        self.nombre = nombre
-        self.especie = especie
-        self.edad = edad
+        self.__nombre = nombre
+        self.__especie = especie
+        self.__edad = edad
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def get_especie(self):
+        return self.__especie
+
+    def set_especie(self, especie):
+        self.__especie = especie
+
+    def get_edad(self):
+        return self.__edad
+
+    def set_edad(self, edad):
+        self.__edad = edad
+
+    def __len__(self):
+        return len(self.__nombre)
 
     def __str__(self):
-        return f"nombre: {self.nombre}\nespecie: {self.especie}\nedad: {self.edad}"
+        return f"nombre: {self.__nombre}\nespecie: {self.__especie}\nedad: {self.__edad}"
